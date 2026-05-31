7e6a8c4517bfe11988114171f4f7aa1d9d41cf6f
praneshnikhar <praneshnikhar@gmail.com>
2026-06-01 00:48:51 +0530

    m back here revising ml

---
+++ fastapi/trainModel.py
@@ -0,0 +1,22 @@
+# from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+
+# X,y = fetch_openml("mnist_784", version=1, return_X_y=True)
+X_train , X_test , y_train , y_test = train_test_split(X,y, test_size=0.2)
+
+clf = RandomForestClassifier(n_jobs=-1)
+clf.fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+
+with open('mnist_model.pkl','wb') as f:
+    pickle.dump(clf,f)
+
